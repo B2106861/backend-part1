4a5516940e2ec0eaaeea2e8f5bc3f01c98b8d0bc
TaMinhTriet <147906169+TaMinhTriet@users.noreply.github.com>
2024-02-22 12:17:50 +0700

    dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req, res) => {
+	res.send({ message: "create hanler" });
+};
+
+exports.findAll = (req, res) => {
+	res.send({ message: "findAll hanler" });
+};
+
+exports.findOne = (req, res) => {
+	res.send({ message: "findOne hanler" });
+};
+
+exports.update = (req, res) => {
+	res.send({ message: "update hanler" });
+};
+
+exports.delete = (req, res) => {
+	res.send({ message: "delete hanler" });
+};
+
+exports.deleteAll = (req, res) => {
+	res.send({ message: "deleteAll hanler" });
+};
+
+exports.findAllFavorite = (req, res) => {
+	res.send({ message: "findALLFavorite hanler" });
+};
