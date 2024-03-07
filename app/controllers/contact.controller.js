34cff65669211fa1b3923e3c28782e304640a13d
TaMinhTriet <147906169+TaMinhTriet@users.noreply.github.com>
2024-03-07 15:24:35 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -53,7 +53,7 @@ exports.findOne = async (req, res, next) => {
 		return next(
 			new ApiError(
 				500,
-				'Error retrieving contact with id=${req.params.id}'
+				`Error retrieving contact with id=${req.params.id}`
 			)
 		);
 	}
@@ -68,13 +68,13 @@ exports.update = async (req, res, next) => {
 	try {
 		const contactService = new ContactService(MongoDB.client);
 		const document = await contactService.update(req.params.id, req.body);
-		if (!document) {
+		if (document) {
 			return next(new ApiError(404, "Contact not found"));
 		}
 		return res.send({ message: "Contact was update successfully" });
 	} catch (error) {
 		return next(
-			new ApiError(500, 'Error updating contact with id=${req.params.id}')
+			new ApiError(500, `Error updating contact with id=${req.params.id}`)
 		);
 	}
 };
@@ -84,15 +84,15 @@ exports.delete = async (req, res, next) => {
 	try {
 		const contactService = new ContactService(MongoDB.client);
 		const document = await contactService.delete(req.params.id);
-		if (!document) {
-			return next(new ApiError(404, "COntact not found"));
+		if (document) {
+			return next(new ApiError(404, "Contact not found"));
 		}
 		return res.send({ message: "Contact was deleted successfully" });
 	} catch (error) {
 		return next(
 			new ApiError(
 				500,
-				'Could not delete contact with id=${req.params.id}'
+				`Could not delete contact with id=${req.params.id}`
 			)
 		);
 	}
@@ -103,7 +103,7 @@ exports.deleteAll = async (_req, res, next) => {
 	try {
 		const contactService = new ContactService(MongoDB.client);
 		const deletedCount = await contactService.deleteAll();
-		return res.send({ message: '${deletedCount} contacts was deleted successfully' });
+		return res.send({ message: `${deletedCount} contacts was deleted successfully` });
 	} catch (error) {
 		return next(
 			new ApiError(
